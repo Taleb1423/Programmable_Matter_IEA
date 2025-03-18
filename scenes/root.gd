4dc3814b7d92101f9da09c89991111107d8d7dfc
souheil-moussa <souheil.imoussa@gmail.com>
2025-03-18 12:27:22 +0200

    improved target selection and naviagtion

--- scenes/root.gd
+++ scenes/root.gd
@@ -2,13 +2,15 @@ extends Node2D
 
 
 class target:
-	var position = Vector2(0,0)
+	var position = Vector2i(0,0)
 	var ocupied  = false
+	var weight = 0
 	
 	
-	static func create(loc:Vector2):
+	static func create(loc:Vector2,w):
 		var ins = target.new()
 		ins.position = loc
+		ins.weight = w
 		return ins
 		
 	
@@ -36,7 +38,7 @@ func _ready() -> void:
 	for y in region_size.y:
 		
 		for x in region_size.x:
-			
+			var w
 			var tile_position=Vector2i(x+region_position.x,region_size.y-1-y +region_position.y)
 			
 			var tile_data = tilemap.get_cell_tile_data(tile_position)
@@ -52,10 +54,14 @@ func _ready() -> void:
 					ins.arrived.connect(_on_agent_arrived)
 					i -=1
 				
-			if tile_data.get_custom_data("target"):				
-				targets.push_back(target.create(tile_position))
-		
-func Astart_path(pos,a1):
+			if tile_data.get_custom_data("target"):
+				targets.push_back(target.create(tile_position,compute_weight(tile_position)))
+			
+	targets.sort_custom(sort_targets)
+	for tar in targets:
+		print(tar.position)	
+	
+func Astart_path(original_pos,pos,a1):
 	var agents = get_tree().get_nodes_in_group("agents")
 	var agents_pos = []
 	for ag in agents:
@@ -65,25 +71,54 @@ func Astart_path(pos,a1):
 	for poss in agents_pos:
 		astar_grid.set_point_solid(poss)	
 	var pout
+	
 	for tar in targets:
 		if not tar.ocupied:
 			pout= astar_grid.get_id_path(pos,tar.position)
+			
+				
 			for poss in agents_pos:
 				astar_grid.set_point_solid(poss,false)
 			return pout
-			
+	pout = astar_grid.get_id_path(pos,pos+Vector2i.DOWN)
 	for poss in agents_pos:
-		astar_grid.set_point_solid(poss,false)	
-	#return astar_grid.get_id_path(pos,pos)
+		astar_grid.set_point_solid(poss,false)
+	
+	return 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 
-func _on_agent_arrived(pos: Variant) -> void:
+func _on_agent_arrived(agent) -> void:
+	print("arrived")
 	for tar in targets:
-		if Vector2i(tar.position) == pos:
-			tar.ocupied = true
-			astar_grid.set_point_solid(tar.position)
-			
+		if not tar.ocupied:
+			if 		agent.global_position== tilemap.map_to_local(tar.position):
+				agent.is_done=true
+				print("arrived at "+str(tar.position))
+				tar.ocupied=true
+				return
+			return
+	
+		
+
+func compute_weight(tile_pos:Vector2i )->int:
+	var w = 0
+	var neighbors=[tilemap.get_cell_tile_data(Vector2i.UP+tile_pos),tilemap.get_cell_tile_data(Vector2i.DOWN+tile_pos),tilemap.get_cell_tile_data(Vector2i.LEFT+tile_pos),tilemap.get_cell_tile_data(Vector2i.RIGHT+tile_pos)]
+	for n in neighbors:
+		if n.get_custom_data("target"):
+			w +=1
+		if not n.get_custom_data("Free"):
+			w+=1	
+	return w
+
+func sort_targets(a,b)->bool:
+	if a.weight > b.weight:
+		return true
+	else:
+		return false
+	
+		
+	 			
