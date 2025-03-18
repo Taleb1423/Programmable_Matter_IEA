7506b4446cd29179c8b975fcf61cd1f34e659781
taleb1423 <taleb.elhousseiny@lau.edu>
2025-03-18 12:09:27 +0200

    enviroment creation implemted

--- scenes/root.gd
+++ scenes/root.gd
@@ -1,89 +1,60 @@
 extends Node2D
 
+@onready var tile_map : TileMapLayer = $Map
 
-class target:
-	var position = Vector2(0,0)
-	var ocupied  = false
+enum PlacementType { NONE, TARGET, OBSTACLE }
+var current_placement_type = PlacementType.NONE
+
+func _ready():
+	# Connect the button signals
+	$Target_Button.pressed.connect(_on_target_button_pressed)
+	$Obstacle_Button.pressed.connect(_on_obstacle_button_pressed)
 	
-	
-	static func create(loc:Vector2):
-		var ins = target.new()
-		ins.position = loc
-		return ins
+func _input(event):
+	if Input.is_action_just_pressed("click") and current_placement_type != PlacementType.NONE:
+		print("Click")
+		var mouse_pos = get_global_mouse_position()
+		print("global mouse pos: ", mouse_pos)
+		var tile_pos = tile_map.local_to_map(mouse_pos)
 		
-	
-	 
-@export var agent_nb = 0
-var astar_grid:AStarGrid2D
-@onready var tilemap= $Map
-var targets=[]
-@onready var agent_scene = preload("res://scenes/agent.tscn")
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	
-	
-	
-	astar_grid = AStarGrid2D.new()
-	astar_grid.region =tilemap.get_used_rect()
-	astar_grid.cell_size =Vector2(32,32)
-	astar_grid.diagonal_mode= AStarGrid2D.DIAGONAL_MODE_NEVER
-	astar_grid.update()
-	var i = agent_nb
-	var region_size = astar_grid.region.size
-	
-	var region_position =astar_grid.region.position
-	
-	for y in region_size.y:
-		
-		for x in region_size.x:
+		# Place the appropriate tile based on the current selection
+		if current_placement_type == PlacementType.TARGET:
+			place_target(tile_pos)
+		elif current_placement_type == PlacementType.OBSTACLE:
+			place_obstacle(tile_pos)
 			
-			var tile_position=Vector2i(x+region_position.x,region_size.y-1-y +region_position.y)
-			
-			var tile_data = tilemap.get_cell_tile_data(tile_position)
-			
-			if tile_data == null or !tile_data.get_custom_data("Free"):
-				astar_grid.set_point_solid(tile_position)
-			else:
-				if i>0:
-					var ins = agent_scene.instantiate()
-					ins.name="agent"+str(i)
-					add_child(ins)
-					ins.position = tilemap.map_to_local(tile_position)
-					ins.arrived.connect(_on_agent_arrived)
-					i -=1
-				
-			if tile_data.get_custom_data("target"):				
-				targets.push_back(target.create(tile_position))
-		
-func Astart_path(pos,a1):
-	var agents = get_tree().get_nodes_in_group("agents")
-	var agents_pos = []
-	for ag in agents:
-		if ag ==a1:
-			continue
-		agents_pos.append(tilemap.local_to_map(ag.global_position))
-	for poss in agents_pos:
-		astar_grid.set_point_solid(poss)	
-	var pout
-	for tar in targets:
-		if not tar.ocupied:
-			pout= astar_grid.get_id_path(pos,tar.position)
-			for poss in agents_pos:
-				astar_grid.set_point_solid(poss,false)
-			return pout
-			
-	for poss in agents_pos:
-		astar_grid.set_point_solid(poss,false)	
-	#return astar_grid.get_id_path(pos,pos)
+	if Input.is_action_just_pressed("right_click"):
+		var mouse_pos = get_global_mouse_position()
+		var tile_pos = tile_map.local_to_map(mouse_pos)
+		remove_tile(tile_pos)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_target_button_pressed():
+	print("Target mode activated")
+	current_placement_type = PlacementType.TARGET
 
+func _on_obstacle_button_pressed():
+	print("Obstacle mode activated")
+	current_placement_type = PlacementType.OBSTACLE
 
-func _on_agent_arrived(pos: Variant) -> void:
-	for tar in targets:
-		if Vector2i(tar.position) == pos:
-			tar.ocupied = true
-			astar_grid.set_point_solid(tar.position)
-			
+func place_target(tile_pos: Vector2i) -> void:
+	var source_id = 0
+	var atlas_coordt = Vector2i(7, 9)
+	
+	tile_map.set_cell(tile_pos, source_id, atlas_coordt)
+	print("Target placed at:", tile_pos)
+
+func place_obstacle(tile_pos: Vector2i) -> void:
+	var source_id = 0
+	var atlas_coordo = Vector2i(3, 6)
+	
+	tile_map.set_cell(tile_pos, source_id, atlas_coordo)
+	print("Obstacle placed at:", tile_pos)
+	
+func remove_tile(tile_pos: Vector2i) -> void:
+	var existing_tile = tile_map.get_cell_tile_data(tile_pos)
+	if existing_tile != null:
+		var source_id = 0
+		var atlas_coordf = Vector2i(3, 4)
+		#tile_map.set_cell(tile_pos, -1)  # Remove the tile
+		tile_map.set_cell(tile_pos, source_id, atlas_coordf)
+		print("Tile removed at:", tile_pos)
